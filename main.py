2f0ce914a2db30c957881bc05f506ab1c28c96a8
Cloud956 <pmlewandowski@gmail.com>
2022-09-22 10:02:34 +0200

    storage init

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from storage_handler import Storage
-
+from storage_handler import Storage, DIR_DATA_CSV, DIR_DATA_JSON
+import glob
 
 
 
 if __name__ == '__main__':
     storage = Storage()
+    json_files=glob.glob(DIR_DATA_JSON+"*.json")
+    print(json_files[0])
