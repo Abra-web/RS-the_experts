65fa0689a77c803d655e96dc72a7dcb23b7fa77f
Tabea Heusel <tabea@rheusel.de>
2022-09-27 12:11:20 +0200

    commented code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import song_searcher as ss
 import pandas as pd
 
 if __name__ == '__main__':
-    data_set_sorted_by_songuri = pd.read_csv(
+    data_set_sorted_by_song_uri = pd.read_csv(
         "C:\\Users\\Tabea Heusel\\PycharmProjects\\RS-the_experts\\data\\csv\\songs.csv")
-    df_songuri = pd.DataFrame(data_set_sorted_by_songuri)
-    df_songuri = df_songuri.set_index('songs').T.to_dict('list')
+    df_song_uris = pd.DataFrame(data_set_sorted_by_song_uri)
+    df_song_uris = df_song_uris.set_index('songs').T.to_dict('list')
 
     data_set_sorted_by_playlist_id = pd.read_csv(
         "C:\\Users\\Tabea Heusel\\PycharmProjects\\RS-the_experts\\data\\csv\\playlists_extract_pid_sort.csv")
@@ -18,7 +18,7 @@ if __name__ == '__main__':
                        'spotify:track:4VrWlk8IQxevMvERoX08iC',
                        'spotify:track:69bp2EbF7Q2rqc5N3ylezZ']
 
-    step1 = ss.song_searcher(input_song_uris, df_songuri)
+    step1 = ss.song_searcher(input_song_uris, df_song_uris)
     step2 = ss.playlist_counter(step1)
     step3 = ss.song_suggester(step2, 5, df_pl_id)
     print(step3)
