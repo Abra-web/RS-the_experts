fa363b6a66d9b8d327a0651994d7c280aff3b1e6
Jan-04 <72268860+Jan-04@users.noreply.github.com>
2022-10-10 12:47:13 +0200

    pickle

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from storage_handler import Storage,join, DIR_DATA_CSV, DIR_DATA_JSON,Type
+from storage_handler import Storage, join, DIR_DATA_CSV, DIR_DATA_JSON, Type
 import glob
 from file_processor import process_all
 
 
 if __name__ == '__main__':
-    storage= Storage()
+    storage = Storage()
     #process_all(storage)
     #storage.extract(100)
     #storage.sort(Type.EXTRACT,'pid')
    # storage.sort(Type.EXTRACT,'track_uri')
     #storage.make_songs(Type.MAIN)
     #storage.drop_columns()
-    storage.pickle_playlist()
-    b=2
+    #storage.pickle_playlist()
